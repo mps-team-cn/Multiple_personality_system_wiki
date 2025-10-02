76af9f52e230d2b710d32f4672ba81c211ee7b9d
lianlian <kuliantnt@gmail.com>
2025-10-03 01:41:23 +0800

    feat: 交换

--- tools/update.cmd
+++ tools/update.cmd
@@ -1,5 +1,5 @@
 python tools/fix_md.py 
 python tools/gen_changelog_by_tags.py  --latest-to-head
-@REM node scripts/gen-last-updated.mjs
+node scripts/gen-last-updated.mjs
 @REM python tools/pdf_export/export_to_pdf.py --pdf-engine=tectonic --cjk-font="Microsoft YaHei" # Windows 
 markdownlint "**/*.md" --ignore "node_modules" --ignore "tools/pdf_export/vendor"
