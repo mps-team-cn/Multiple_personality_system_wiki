3b4562d882d2d0ae11ac45707cb109adb9deb355
lianlian <kuliantnt@gmail.com>
2025-10-03 12:13:29 +0800

    脚本修改

--- tools/update.cmd
+++ tools/update.cmd
@@ -1,5 +1,5 @@
 python tools/fix_md.py 
 python tools/gen_changelog_by_tags.py  --latest-to-head
-node scripts/gen-last-updated.mjs
-@REM python tools/pdf_export/export_to_pdf.py --pdf-engine=tectonic --cjk-font="Microsoft YaHei" # Windows 
+@REM node scripts/gen-last-updated.mjs
+python tools/pdf_export/export_to_pdf.py --pdf-engine=tectonic --cjk-font="Microsoft YaHei" 
 markdownlint "**/*.md" --ignore "node_modules" --ignore "tools/pdf_export/vendor"
