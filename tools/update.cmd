926dd17545bbf22b560f6f7f695aad0001f4cac1
lianlian <kuliantnt@gmail.com>
2025-10-04 07:34:36 +0800

    chore: 删除更新脚本 update.cmd

--- tools/update.cmd
+++
@@ -1,7 +0,0 @@
-python tools/fix_md.py 
-python tools/gen_changelog_by_tags.py  --latest-to-head
-python tools/retag_and_related.py
-node scripts/gen-last-updated.mjs
-python tools/pdf_export/export_to_pdf.py --pdf-engine=tectonic --cjk-font="Microsoft YaHei" 
-markdownlint "**/*.md" --ignore "node_modules" --ignore "tools/pdf_export/vendor"
-python generate_tags_index
